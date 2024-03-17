c3ca85395ae13142cbd0f61d17523af928ab20fa
EremK <105379532+EremK@users.noreply.github.com>
2024-03-17 21:31:33 +0100

    Task #10 done.

--- lab3/main.py
+++ lab3/main.py
@@ -1,14 +1,14 @@
 # Task 1
-# squares_list = [x ** 2 for x in range(1, 11)]
-# print(squares_list)
+squares_list = [x ** 2 for x in range(1, 11)]
+print(squares_list)
 
 
 # Task 2
-# def e_squares(start, end):
-#     return [x ** 2 for x in range(start, end)]
-#
-#
-# print(e_squares(3, 15))
+def e_squares(start, end):
+    return [x ** 2 for x in range(start, end)]
+
+
+print(e_squares(3, 15))
 
 
 # Task 3
@@ -37,13 +37,12 @@ class SquareGenerator:
 
 
 # Task 6
-# main.py
-# from square_generator import SquareGenerator
-#
-# try:
-#     print(SquareGenerator.e_squares(5, 21))
-# except ValueError as e:
-#     print(e)
+from square_generator import SquareGenerator
+
+try:
+    print(SquareGenerator.e_squares(5, 21))
+except ValueError as e:
+    print(e)
 
 # Task 7
 from square_generator.square_generator import SquareGenerator
@@ -55,15 +54,15 @@ except ValueError as e:
 
 
 # Task 8
-# class CubicGenerator(SquareGenerator):
-#     @staticmethod
-#     def e_cubes(start, end):
-#         if start > end:
-#             raise ValueError("Start index must be smaller than the end one.")
-#         return [x ** 3 for x in range(start, end)]
-#
-#
-# print(CubicGenerator.e_cubes(1, 11))
+class CubicGenerator(SquareGenerator):
+    @staticmethod
+    def e_cubes(start, end):
+        if start > end:
+            raise ValueError("Start index must be smaller than the end one.")
+        return [x ** 3 for x in range(start, end)]
+
+
+print(CubicGenerator.e_cubes(1, 11))
 
 # Task 9
 class CubicGenerator(SquareGenerator):
@@ -72,3 +71,22 @@ class CubicGenerator(SquareGenerator):
         if start > end:
             raise ValueError("Start index must be smaller than the end one.")
         return [x ** 2 for x in range(start, end)]
+
+
+# Task 10
+from abc import ABC, abstractmethod
+
+
+class SquareGenerator(ABC):
+
+    @abstractmethod
+    def gen_squares(self, start, end):
+        pass
+
+
+class CubicGenerator(SquareGenerator):
+
+    def gen_squares(self, start, end):
+        if start > end:
+            raise ValueError("Start index must be smaller than the end one.")
+        return [x ** 3 for x in range(start, end)]
