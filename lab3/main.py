b92246f44dc33f9c52ec66e2ad020f34df131a1b
EremK <105379532+EremK@users.noreply.github.com>
2024-03-17 21:17:38 +0100

    Task #9 done.

--- lab3/main.py
+++ lab3/main.py
@@ -55,12 +55,20 @@ except ValueError as e:
 
 
 # Task 8
+# class CubicGenerator(SquareGenerator):
+#     @staticmethod
+#     def e_cubes(start, end):
+#         if start > end:
+#             raise ValueError("Start index must be smaller than the end one.")
+#         return [x ** 3 for x in range(start, end)]
+#
+#
+# print(CubicGenerator.e_cubes(1, 11))
+
+# Task 9
 class CubicGenerator(SquareGenerator):
     @staticmethod
-    def e_cubes(start, end):
+    def e_squares(start, end):
         if start > end:
             raise ValueError("Start index must be smaller than the end one.")
-        return [x ** 3 for x in range(start, end)]
-
-
-print(CubicGenerator.e_cubes(1, 11))
+        return [x ** 2 for x in range(start, end)]
