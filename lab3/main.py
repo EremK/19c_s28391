2a9f44427a1994cb6a8502dfe1c1becc1396f252
EremK <105379532+EremK@users.noreply.github.com>
2024-03-17 20:43:52 +0100

    Task #2 done.

--- lab3/main.py
+++ lab3/main.py
@@ -1,4 +1,11 @@
 # Task 1
-squares_list = [x**2 for x in range(1, 11)]
-print(squares_list)
+# squares_list = [x ** 2 for x in range(1, 11)]
+# print(squares_list)
 
+
+# Task 2
+def e_squares(start, end):
+    return [x ** 2 for x in range(start, end)]
+
+
+print(e_squares(3, 15))
